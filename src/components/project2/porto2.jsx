a9e3431a0b248e4e2aadcf59d072be8cc4c81f8b
AzraYazid <azrayazidalkautsar@gmail.com>
2022-08-25 11:58:40 +0700

    update portt

--- src/components/project2/porto2.jsx
+++ src/components/project2/porto2.jsx
@@ -1,11 +1,88 @@
+// import React from 'react';
+// import './porto2.css';
+
+// import thumbnail1 from '../../img/thumbnails/1.jpg';
+// // import thumbnail2 from '../../img/thumbnails/2.jpg';
+// import thumbnail3 from '../../img/thumbnails/3.jpg';
+// import thumbnail4 from '../../img/thumbnails/q1.png';
+// import thumbnail5 from '../../img/thumbnails/q2.png';
+
+// export default function Porto2() {
+//   return (
+//     <div id="portfolio">
+//       <h1 className="text-center p-2" id="Projects" style={{ backgroundColor: '#ffd300' }}>
+//         PROJECTS
+//       </h1>
+//       <div class="container-fluid p-0">
+//         <div class="row g-0">
+//           {/* THUMBNAIL UKURAN 650X350 */}
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail1} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-black-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail5} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-black-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail3} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-black-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail1} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-white-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail3} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-white-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//           <div class="col-lg-4 col-sm-6">
+//             <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
+//               <img class="img-fluid" src={thumbnail4} alt="..." />
+//               <div class="portfolio-box-caption">
+//                 <div class="project-category text-white-50">Category</div>
+//                 <div class="project-name">Project Name</div>
+//               </div>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React from 'react';
 import './porto2.css';
 
 import thumbnail1 from '../../img/thumbnails/1.jpg';
-// import thumbnail2 from '../../img/thumbnails/2.jpg';
-import thumbnail3 from '../../img/thumbnails/3.jpg';
+import thumbnail2 from '../../img/thumbnails/2.jpg';
+import thumbnail3 from '../../img/thumbnails/q2.png';
 import thumbnail4 from '../../img/thumbnails/q1.png';
-import thumbnail5 from '../../img/thumbnails/q2.png';
 
 export default function Porto2() {
   return (
@@ -15,7 +92,6 @@ export default function Porto2() {
       </h1>
       <div class="container-fluid p-0">
         <div class="row g-0">
-          {/* THUMBNAIL UKURAN 650X350 */}
           <div class="col-lg-4 col-sm-6">
             <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
               <img class="img-fluid" src={thumbnail1} alt="..." />
@@ -27,7 +103,7 @@ export default function Porto2() {
           </div>
           <div class="col-lg-4 col-sm-6">
             <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
-              <img class="img-fluid" src={thumbnail5} alt="..." />
+              <img class="img-fluid" src={thumbnail2} alt="..." />
               <div class="portfolio-box-caption">
                 <div class="project-category text-black-50">Category</div>
                 <div class="project-name">Project Name</div>
